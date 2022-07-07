7ab7da6a881f459e91918a4bef591475d2b8a95c
yazdan bahadori <yzdanbhadry@gmail.com>
2022-07-07 17:12:09 +0430

    test

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
     button.resize(200, 100);
     button.show();
     return QApplication::exec();
