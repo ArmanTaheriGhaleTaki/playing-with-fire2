cb02857a10452b772a52b5fb4cd304e90c5f25b5
Arman <armantahery1381@gmail.com>
2022-07-12 15:01:00 +0430

    bricklaying is done

---
+++ box.cpp
@@ -0,0 +1,6 @@
+#include "box.h"
+
+Box::Box()
+{
+
+}
---
+++ src/views/block.h
@@ -0,0 +1,12 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include<QGraphicsPixmapItem>
+
+class Block: public QGraphicsPixmapItem{
+
+public:
+    Block(int width , int height);
+};
+
+#endif // BLOCK_H
