1c7fcbeda6bd1e0b78b32551e017b2074911086f
yazdan bahadori <yzdanbhadry@gmail.com>
2022-07-19 18:38:57 +0430

    add health and score

--- src/Player/Player.h
+++ src/Player/Player.h
@@ -2,18 +2,21 @@
 #define PLYING_WITH_FIRE2_PLAYER_H
 
 #include <QGraphicsPixmapItem>
+
 class Player : public QGraphicsPixmapItem {
 private:
 
     int width{};
     int height{};
     int HitPoint{3};
+    int score{0};
 public:
 
     int NumberOfBombs{};
 
     Player(const char* icon,int width,int height,int NumberOfBombs);
-
+    void DecreaseHealth();
+    void increaseScore();
     QRectF boundingRect() const override;
 
 
