9e1c080e72d5c3903ba9fef462d56cb70903dcac
yazdan bahadori <yzdanbhadry@gmail.com>
2022-07-19 18:38:57 +0430

    add health and score

--- src/Player/Player.cpp
+++ src/Player/Player.cpp
@@ -1,6 +1,8 @@
 #include "Player.h"
+#include "../Home/Game.h"
 
 #include <QGraphicsTextItem>
+
 Player::Player(const char* icon,int width,int height,int NumberOfBombs): width(width),height(height),NumberOfBombs{NumberOfBombs} {
 
     QPixmap pixmap(icon);
@@ -15,4 +17,18 @@ QRectF Player::boundingRect() const {
     player.setWidth(width);
     player.setHeight(height);
     return QGraphicsPixmapItem::boundingRect();
+}
+void Player::DecreaseHealth()
+{
+    HitPoint--;
+    if(HitPoint == 0)
+    {
+        scene()->removeItem(this);
+        delete this;
+    }
+}
+
+void Player::increaseScore()
+{
+    score+=25;
 }
